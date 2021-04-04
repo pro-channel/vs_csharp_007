c90a3215eb729290287b3c04df03e220bd49bf3c
プロちゃん <->
2021-04-04 20:25:13 +0900

    ソースコード追加

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace vs_csharp_007
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.buttonIndex = new System.Windows.Forms.Button();
+            this.buttonValue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(249, 107);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 20);
+            this.comboBox.TabIndex = 0;
+            this.comboBox.SelectionChangeCommitted += new System.EventHandler(this.comboBox_SelectionChangeCommitted);
+            // 
+            // buttonIndex
+            // 
+            this.buttonIndex.Location = new System.Drawing.Point(193, 176);
+            this.buttonIndex.Name = "buttonIndex";
+            this.buttonIndex.Size = new System.Drawing.Size(75, 23);
+            this.buttonIndex.TabIndex = 1;
+            this.buttonIndex.Text = "index";
+            this.buttonIndex.UseVisualStyleBackColor = true;
+            this.buttonIndex.Click += new System.EventHandler(this.buttonIndex_Click);
+            // 
+            // buttonValue
+            // 
+            this.buttonValue.Location = new System.Drawing.Point(330, 176);
+            this.buttonValue.Name = "buttonValue";
+            this.buttonValue.Size = new System.Drawing.Size(75, 23);
+            this.buttonValue.TabIndex = 2;
+            this.buttonValue.Text = "value";
+            this.buttonValue.UseVisualStyleBackColor = true;
+            this.buttonValue.Click += new System.EventHandler(this.buttonValue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 224);
+            this.Controls.Add(this.buttonValue);
+            this.Controls.Add(this.buttonIndex);
+            this.Controls.Add(this.comboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button buttonIndex;
+        private System.Windows.Forms.Button buttonValue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vs_csharp_007
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// formのロードイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("name");
+
+            DataRow dr = dt.NewRow();
+            dr["id"] = "10";
+            dr["name"] = "肉";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["id"] = "11";
+            dr["name"] = "魚";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["id"] = "12";
+            dr["name"] = "野菜";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["id"] = "13";
+            dr["name"] = "果物";
+            dt.Rows.Add(dr);
+
+            this.comboBox.DataSource = dt;
+            this.comboBox.DisplayMember = "name";
+            this.comboBox.ValueMember = "id";
+
+
+        }
+
+        /// <summary>
+        /// コンボボックスの選択イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void comboBox_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            int index = this.comboBox.SelectedIndex;
+            string value = this.comboBox.SelectedValue.ToString();
+
+            return;
+        }
+
+        /// <summary>
+        /// indexボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonIndex_Click(object sender, EventArgs e)
+        {
+            // 魚を選択
+            this.comboBox.SelectedIndex = 1;
+        }
+
+        /// <summary>
+        /// valueボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonValue_Click(object sender, EventArgs e)
+        {
+            // 果物を選択
+            this.comboBox.SelectedValue = "13";
+        }
+    }
+}
